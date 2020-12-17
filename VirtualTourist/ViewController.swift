2e940c673c775e21aa8b3e9dab6ebc4822646bb1
Moran, Shawn (sm857p) <sm857p@us.att.com>
2020-12-16 23:59:13 -0700

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Shawn Moran on 12/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
